7291f1cda143e0a3d3b086baf5c330330a7c0818
Steven Haesler <hasv@zhaw.ch>
2025-04-16 11:18:17 +0200

    adapt compose file

--- client_consumer/consumer.py
+++ client_consumer/consumer.py
@@ -2,20 +2,25 @@ import cv2
 import base64
 import numpy as np
 from kafka import KafkaConsumer
+import time
 
-consumer = KafkaConsumer(
-    'video-stream-processed',
-    bootstrap_servers='localhost:9092',
-    auto_offset_reset='latest'
-)
+while True:
+    time.sleep(1)  # Wait for Kafka to be ready
+    consumer = KafkaConsumer(
+        'video-stream-processed',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset='latest'
+    )
+    
 
-for msg in consumer:
-    jpg_original = base64.b64decode(msg.value)
-    np_arr = np.frombuffer(jpg_original, dtype=np.uint8)
-    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    for msg in consumer:
+        print(f"msg: {msg}")
+        jpg_original = base64.b64decode(msg.value)
+        np_arr = np.frombuffer(jpg_original, dtype=np.uint8)
+        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-    cv2.imshow("Processed Video Stream", frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow("Processed Video Stream", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()

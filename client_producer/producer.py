eb6c0539be07f14f4879c81b109dcb23fe4f42f5
Steven Haesler <hasv@zhaw.ch>
2025-04-16 11:07:23 +0200

    change docker-compose file

--- client_producer/producer.py
+++ client_producer/producer.py
@@ -3,21 +3,28 @@ import base64
 from kafka import KafkaProducer
 import time
 
-producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 def encode_frame(frame):
     _, buffer = cv2.imencode('.jpg', frame)
     return base64.b64encode(buffer)
 
 cap = cv2.VideoCapture(0)
+producer = None
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+    if producer is None:
+        try:
+            time.sleep(1)  
+            producer = KafkaProducer(bootstrap_servers='localhost:9092')
+        except Exception as e:
+            print(f"Error connecting to Kafka: {e}")
     encoded_frame = encode_frame(frame)
-    producer.send('video-stream-raw', encoded_frame)
-    producer.flush()
-    time.sleep(0.05)
+    if producer is not None:
+        producer.send('video-stream-raw', encoded_frame)
+        producer.flush()
+        time.sleep(0.05)
 
 cap.release()
